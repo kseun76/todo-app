b42f56032d9597a622f9d74bc4f8c702d74ea383
은광성 <kseun@hanatour.com>
2024-05-08 18:26:49 +0900

    - 할일 입력하지 않았을 경우 다이얼로 경고창이 뜨도록 수정

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -8,21 +8,42 @@
     <span class="addContainer" v-on:click="addTodo">
       <i class="addBtn fas fa-plus" aria-hidden="true"></i>
     </span>
+    <customModal v-if="showModal" @close="showModal = false">
+      <template #header>
+        <h3>경고</h3>
+      </template>
+      <template #footer>
+          <span @click="showModal = false">할 일을 입력하세요.
+            <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
+          </span>
+      </template>
+    </customModal>
   </div>
 </template>
 <script>
+import CustomModal from "@/components/common/CustomModal.vue";
+
 export default {
+  components: {
+    customModal: CustomModal
+  },
   data() {
     return {
       inputText: '',
+      showModal: false
     };
   },
   methods: {
     addTodo: function () {
-      var value = this.inputText;
-      // localStorage.setItem(value, value);
-      this.$emit('add-todo', value);
-      this.clearInput();
+
+      if (this.inputText !== '') {
+        var value = this.inputText;
+        // localStorage.setItem(value, value);
+        this.$emit('add-todo', value);
+        this.clearInput();
+      } else {
+        this.showModal = !this.showModal;
+      }
     },
     clearInput: function () {
       this.inputText = '';
